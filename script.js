edeb9e110543381b255fb6fe4c8899bee192d142
Marcelo Vitaliano <hmencuadernaciones@gmail.com>
2025-04-17 13:13:05 -0600

    Diseño final profesional y funcional de HM Producción

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById("addTrabajo").addEventListener("click", () => {
+  alert("Funcionalidad próximamente");
+});
+document.querySelectorAll(".actualizar").forEach(boton => {
+  boton.addEventListener("click", () => alert("Actualizar trabajo"));
+});
+document.getElementById("enviarCliente").addEventListener("click", () => {
+  alert("PDF enviado al cliente");
+});
